015b68a9fd6a6254093bae8178ab10e2578ff831
igorcmoura <igorcmgamer@gmail.com>
2016-09-08 13:18:58 -0300

    Fix logging to show python exceptions

--- classes/logger.py
+++ classes/logger.py
@@ -3,16 +3,12 @@
 
 import logging
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-console_logger = logging.StreamHandler()
-console_logger.setLevel(logging.INFO)
-console_logger.setFormatter(log_format)
-logger.addHandler(console_logger)
-
 file_logger = logging.FileHandler('info.log')
 file_logger.setLevel(logging.INFO)
 file_logger.setFormatter(log_format)
